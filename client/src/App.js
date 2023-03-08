640a8bf150460598491574219061d65800068501
Tooboi <japica123@gmail.com>
2023-03-08 17:17:34 -0500

    Format App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
+import Home from './pages/Home';
+import Nav from './components/Nav/Nav';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
