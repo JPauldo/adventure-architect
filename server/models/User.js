ece5a33403324182b1a07466bb77fddeeedcf13c
KingdomSeeker328 <jdpauldo94@gmail.com>
2023-03-08 17:49:01 -0500

    Add bcrypt and fullName Virtual

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
@@ -50,6 +51,31 @@ const userSchema = new Schema(
     },
   }
 );
+
+userSchema.virtual('fullName')
+  // Getter
+  .get(function () {
+    return `${this.firstName} ${this.lastName}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const firstName = v.split(' ')[0];
+    const lastName = v.split(' ')[1];
+    this.set({ first: firstName, last: lastName });
+  });
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
