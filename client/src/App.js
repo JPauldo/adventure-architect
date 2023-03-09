f09169e6a402d4294448efd15996145fba7480c4
Tooboi <japica123@gmail.com>
2023-03-08 21:47:11 -0500

    Login & Signup pages created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Components
 import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Nav from './components/Nav/Nav';
 
 const App = () => {
@@ -12,6 +15,9 @@ const App = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
     </div>
