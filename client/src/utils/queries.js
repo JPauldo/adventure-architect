abfdc658b630ebb913bf26b72ef460f5f9c3aea0
Brian Chapman <chapman.brian86@gmail.com>
2023-03-10 16:13:32 -0500

    Working on queries for front-end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,38 @@
 import { gql } from '@apollo/client';
 
+
+export const QUERY_ME = gql`
+    query getMe {
+        _id
+        firstName
+        lastName
+        # Confirm if bucket is wanted here.It's not in getMe resolvers.
+        bucket {
+            name
+            location
+        }
+        trips {
+            _id
+        }
+    } 
+`;
+
+export const QUERY_SINGLE_USER  = gql`
+    query getSingleUser(firstName: String!, lastName: String!) {
+        user(firstName: $firstName, lastname: $lastName) {
+            _id
+        firstName
+        lastName
+        # Confirm if bucket is wanted here.It's not in getMe resolvers.
+        bucket {
+            name
+            location
+        }
+        trips {
+            _id
+        }
+      } 
+   }
+`;
+
+
